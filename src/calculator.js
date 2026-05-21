ab3bd631bd2d4c2049496603210b210824fcf0b1
Lamija Altumbabic <lamija.altumbabic@fet.ba>
2026-05-21 18:32:52 +0200

    Dodat kalkulator

---
+++ src/calculator.js
@@ -0,0 +1,15 @@
+var==5;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = { add, subtract, multiply };
